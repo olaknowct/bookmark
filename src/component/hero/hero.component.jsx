01c6c8ccfc66653b2a929b3061b4e0796f2ffb0d
Christopher Olano <olanochristopher@gmail.com>
2023-04-20 15:21:04 +0800

    hero responsive

--- src/component/hero/hero.component.jsx
+++ src/component/hero/hero.component.jsx
@@ -1,27 +1,27 @@
 import heroImg from '../../images/illustration-hero.svg';
-import { ReactComponent as HeroImg } from '../../images/illustration-hero.svg';
 
 const Hero = () => {
   return (
     <section className=''>
-      <div className='grid grid-cols-2 gap-[2.2rem] pt-[5.53rem] px-[2rem]'>
-        <div className='pt-[7.874rem] text-veryDarkBlue max-w-[54.4rem] ml-auto'>
-          <h1 className='text-[4.7rem] font-semibold leading-[5.2rem]'>
+      <div className='grid lg:grid-cols-2 gap-[2.2rem] pt-[5.53rem] px-[2rem]'>
+        <div className='xl:pt-[7.874rem] order-2 lg:order-1 lg:pt-[2rem] flex flex-col text-veryDarkBlue max-w-[60rem] mx-auto text-center lg:text-left lg:max-w-[45rem] xl:max-w-[54.4rem] lg:mx-0 lg:ml-auto'>
+          <h1 className='xl:text-[4.7rem] text-[4rem] font-semibold leading-[5.2rem]'>
             A Simple Bookmark Manager
           </h1>
           <p className='text-[1.8rem] mt-[2.6rem] tracking-[0.01rem] opacity-60'>
             A clean and simple interface to organize your favourite websites. Open a new browser tab
             and see your sites load instantly. Try it for free
           </p>
-          <button className='bg-softBlue text-white mt-[3.3rem] text-[1.5rem] px-[2.2rem] pb-[1.43rem] pt-[1.3rem] tracking-[0.02rem] rounded-lg'>
-            Get it on Chrome
-          </button>
-          <button className='bg-grayishBlue text-veryDarkBlue text-[1.5rem] ml-[2rem] font-bold text-opacity-70 bg-opacity-10 font-inherit px-[2rem] pb-[1.43rem] pt-[1.3rem] rounded-lg shadow-md'>
-            Get it on firefox
-          </button>
+          <div>
+            <button className='bg-softBlue text-white mt-[3.3rem] text-[1.5rem] px-[2.2rem] pb-[1.43rem] pt-[1.3rem] tracking-[0.02rem] rounded-lg'>
+              Get it on Chrome
+            </button>
+            <button className='bg-grayishBlue text-veryDarkBlue text-[1.5rem] ml-[2rem] font-bold text-opacity-70 bg-opacity-10 font-inherit px-[2rem] pb-[1.43rem] pt-[1.3rem] rounded-lg shadow-md'>
+              Get it on firefox
+            </button>
+          </div>
         </div>
-        <div className="after:content-[''] after:bg-softBlue after:h-[35.2rem] after:rounded-full after:w-[1000rem] after:block after:absolute after:top-[20rem] after:left-[92.5rem] after:-z-10">
-          {/* <HeroImg /> */}
+        <div className="after:content-[''] justify-self-center order-1 lg:order-2 after:bg-softBlue after:h-[35.2rem] after:rounded-full after:w-[1000rem] after:block after:absolute lg:after:top-[20rem] after:top-[15rem] after:left-[20rem] lg:after:left-[92.5rem] after:-z-10">
           <img src={heroImg} width={657} height={466} />
         </div>
       </div>
