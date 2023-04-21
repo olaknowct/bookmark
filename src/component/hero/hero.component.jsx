3397f8607ceecd4ed448ba631b422f88daece71a
Christopher Olano <olanochristopher@gmail.com>
2023-04-21 20:07:36 +0800

    use generic stlyes h1

--- src/component/hero/hero.component.jsx
+++ src/component/hero/hero.component.jsx
@@ -5,15 +5,13 @@ const Hero = () => {
     <section className=''>
       <div className='grid lg:grid-cols-2 gap-[2.2rem] pt-[5.53rem] px-[2rem]'>
         <div className='xl:pt-[7.874rem] order-2 lg:order-1 lg:pt-[2rem] flex flex-col text-veryDarkBlue max-w-[60rem] mx-auto text-center lg:text-left lg:max-w-[45rem] xl:max-w-[54.4rem] lg:mx-0 lg:ml-auto'>
-          <h1 className='xl:text-[4.7rem] text-[3rem] xs:text-[3.5rem] sm:text-[4rem] font-semibold leading-[5.2rem]'>
-            A Simple Bookmark Manager
-          </h1>
+          <h1>A Simple Bookmark Manager</h1>
           <p className='sm:text-[1.8rem] text-[1.5rem] mt-[2.6rem]  tracking-[0.01rem] opacity-60'>
             A clean and simple interface to organize your favourite websites. Open a new browser tab
             and see your sites load instantly. Try it for free
           </p>
           <div className='mt-[3.3rem] flex flex-col xs:flex-row gap-[2rem] justify-center lg:justify-start '>
-            <button className='bg-softBlue text-white text-[1.5rem] px-[2.2rem] pb-[1.43rem] pt-[1.3rem] tracking-[0.02rem] rounded-lg'>
+            <button className='bg-softBlue text-white text-[1.5rem] px-[2.2rem] pb-[1.43rem] pt-[1.3rem] tracking-[0.02rem] rounded-lg font-bold'>
               Get it on Chrome
             </button>
             <button className='bg-grayishBlue text-veryDarkBlue  text-[1.5rem]  font-bold text-opacity-70 bg-opacity-10 font-inherit px-[2rem] pb-[1.43rem] pt-[1.3rem] rounded-lg shadow-md'>
