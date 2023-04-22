9d4c1b67ab8e9ef2f526c6ed2f89c7b69cdae587
Christopher Olano <olanochristopher@gmail.com>
2023-04-22 18:23:05 +0800

    faqs heading

---
+++ src/component/faqs/faqs.component.jsx
@@ -0,0 +1,15 @@
+const Faqs = () => {
+  return (
+    <section>
+      <div className='mt-[12.3rem] flex flex-col gap-[1.6rem] pt-[3.3rem] text-center'>
+        <h2>Frequently Asked Questions</h2>
+        <p className='section-p-default leading-[2.8rem] tracking-[0.002rem]'>
+          Here are some of our FAQs. If you have any other questions you'd like answered please feel
+          free to email us.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Faqs;
--- src/component/main/main.component.jsx
+++ src/component/main/main.component.jsx
@@ -1,5 +1,6 @@
 import { FeatureProvider } from '../../context/features.context';
 import Extensions from '../extensions/extensions.component';
+import Faqs from '../faqs/faqs.component';
 import Features from '../features/features.component';
 import Hero from '../hero/hero.component';
 
@@ -11,6 +12,7 @@ const Main = () => {
         <Features />
       </FeatureProvider>
       <Extensions />
+      <Faqs />
     </main>
   );
 };
