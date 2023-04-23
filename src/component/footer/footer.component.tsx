f59d50d0e14bda449c6ab05dde482d23312afaf0
Christopher Olano <olanochristopher@gmail.com>
2023-04-24 00:29:30 +0800

    footer link active state

--- src/component/footer/footer.component.tsx
+++ src/component/footer/footer.component.tsx
@@ -1,6 +1,6 @@
 import { ReactComponent as BookmarkLogo } from '../../images/logo-bookmark-light.svg';
-import { ReactComponent as FacebookLogo } from '../../images/icon-facebook.svg';
-import { ReactComponent as TwitterLogo } from '../../images/icon-twitter.svg';
+import FacebookLogo from '../../images/icon-facebook.svg';
+import TwitterLogo from '../../images/icon-twitter.svg';
 
 const Footer = () => {
   console.log('render footer');
@@ -12,16 +12,16 @@ const Footer = () => {
           <BookmarkLogo />
         </a>
         <ul className='flex flex-col gap-[4.4rem] text-[1.2rem] uppercase tracking-[0.2rem] md:flex-row '>
-          <li>Features</li>
-          <li>Pricing</li>
-          <li className='pl-[0.4rem]'>Contact</li>
+          <li className='hover-li'>Features</li>
+          <li className='hover-li'>Pricing</li>
+          <li className='hover-li pl-[0.4rem]'>Contact</li>
         </ul>
         <ul className='flex flex-row gap-[4rem] md:ml-auto'>
           <li>
-            <FacebookLogo />
+            <img src={FacebookLogo} alt='FacebookLogo' className='cursor-pointer filter' />
           </li>
           <li>
-            <TwitterLogo />
+            <img src={TwitterLogo} alt='TwitterLogo' className='cursor-pointer filter' />
           </li>
         </ul>
       </div>
